cbaec9640c7aad0ceaf18b1a54f47110946c82a0
jkpxT14 <jkpxtfin@gmail.com>
2025-04-03 11:56:52 +0900

    Commit

--- Programming & Problem solving (CSED101)/Labs/20250205_Lab2_2.py
+++
@@ -1,6 +0,0 @@
-inputMoney=int(input("투입한 돈: "))
-price=int(input("물건 값: "))
-change=inputMoney-price
-print("거스름돈: {}원".format(change))
-print("500원짜리: {}개".format(change//500))
-print("100원짜리: {}개".format((change%500)//100))
