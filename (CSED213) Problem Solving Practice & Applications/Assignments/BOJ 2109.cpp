110c08e1a756d6cc6c5ee742554099e8e3217a1f
jkpxT14 <jkpxtfin@gmail.com>
2025-10-04 10:08:02 +0900

    Commit

--- (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 2109.cpp
+++ (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 2109.cpp
@@ -1,4 +1,4 @@
-// BOJ 2109: Supermarket
+// BOJ 2109: Supermarket(순회강연)
 
 // #include <bits/stdc++.h>
 
@@ -7,7 +7,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
-#include <deque> // DEQUE = Double-Ended QUEue
+#include <deque>
 #include <list>
 #include <set>
 #include <map>
@@ -18,6 +18,7 @@
 // #include <limits>
 #include <cmath>
 #include <bitset>
+#include <functional>
 
 using namespace std;
 
@@ -31,6 +32,10 @@ using vb=vector<bool>;
 // using qi=queue<int>;
 using dqi=deque<int>;
 using dqll=deque<long long>;
+using pqi=priority_queue<int>;
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqll=priority_queue<long long>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
 using pii=pair<int, int>;
 using vvi=vector<vector<int>>;
 using vpii=vector<pair<int, int>>;
@@ -44,6 +49,7 @@ using vvpii=vector<vector<pair<int, int>>>;
 #define LLONG_MIN -9223372036854775808
 
 // const int INF=INT_MAX/4;
+// const ll mod=1000000007; // 10^9+7
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -52,6 +58,9 @@ int main(){
     
     cout<<fixed;
     cout.precision(10);
+
+    int n;
+    cin>>n;
 
     return 0;
 }
