bb8045eb7ba71547c54a1acbeb4fb6bcad5ffd6c
jkpxT14 <jkpxtfin@gmail.com>
2025-10-06 19:51:26 +0900

    Commit

---
+++ (CSED001) Competitive Programming/KOI/2019_1_M_2 Sol 2.cpp
@@ -0,0 +1,95 @@
+// BOJ 17611: 직각다각형
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <tuple>
+// #include <limits>
+#include <cmath>
+#include <bitset>
+#include <functional>
+
+using namespace std;
+
+using ll=long long;
+using vi=vector<int>;
+using vll=vector<long long>;
+using vd=vector<double>;
+using vb=vector<bool>;
+// using sti=stack<int>;
+// using stch=stack<char>;
+// using qi=queue<int>;
+using dqi=deque<int>;
+using dqll=deque<long long>;
+using pqi=priority_queue<int>;
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqll=priority_queue<long long>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+using pii=pair<int, int>;
+using vvi=vector<vector<int>>;
+using vpii=vector<pair<int, int>>;
+using vpipii=vector<pair<int, pair<int, int>>>;
+using dqpii=deque<pair<int, int>>;
+using vvpii=vector<vector<pair<int, int>>>;
+
+#define INT_MAX 2147483647 // = 2^31-1
+#define INT_MIN -2147483648
+#define LLONG_MAX 9223372036854775807 // = 2^63-1
+#define LLONG_MIN -9223372036854775808
+
+// const int INF(INT_MAX/4);
+// const ll mod(1000000007); // 10^9+7
+// const int offset(500000);
+
+int f(vpii& L){
+    sort(L.begin(), L.end());
+    pqig pq;
+    int ret(-1);
+    for(int i=0; i<L.size(); ++i){
+        while(!pq.empty() && pq.top()<=L[i].first) pq.pop();
+        pq.push(L[i].second);
+        ret=max(ret, (int)pq.size());
+    }
+    return ret;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cout<<fixed;
+    cout.precision(10);
+
+    int n;
+    cin>>n;
+    vpii coord(n+1);
+    for(int i=0; i<n; ++i){
+        cin>>coord[i].first>>coord[i].second;
+    }
+    coord[n]=coord[0];
+    vpii H, V;
+    for(int i=0; i<n; ++i){
+        if(coord[i].first==coord[i+1].first){
+            H.push_back({min(coord[i].second, coord[i+1].second), max(coord[i].second, coord[i+1].second)});
+        }
+        else if(coord[i].second==coord[i+1].second){
+            V.push_back({min(coord[i].first, coord[i+1].first), max(coord[i].first, coord[i+1].first)});
+        }
+    }
+    int h(f(H)), v(f(V));
+    cout<<max(h, v);
+
+    return 0;
+}
