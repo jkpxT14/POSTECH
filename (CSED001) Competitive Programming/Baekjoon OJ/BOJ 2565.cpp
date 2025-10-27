aaa84417b901b953468b4ba8ef691c97195369ab
jkpxT14 <jkpxtfin@gmail.com>
2025-10-27 11:40:19 +0900

    Commit

---
+++ (CSED001) Competitive Programming/Baekjoon OJ/BOJ 2565.cpp
@@ -0,0 +1,86 @@
+// BOJ 2565: 전깃줄
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <tuple>
+#include <climits>
+#include <limits>
+#include <cmath>
+#include <bitset>
+#include <functional>
+#include <cassert>
+
+using namespace std;
+
+using ll=long long;
+using vi=vector<int>;
+using vll=vector<long long>;
+using vd=vector<double>;
+using vb=vector<bool>;
+using vstr=vector<string>;
+// using sti=stack<int>;
+// using stch=stack<char>;
+// using qi=queue<int>;
+using dqi=deque<int>;
+using dqll=deque<long long>;
+using pqi=priority_queue<int>;
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqll=priority_queue<long long>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+using pii=pair<int, int>;
+using ppiii=pair<pair<int, int>, int>;
+using pipii=pair<int, pair<int, int>>;
+using pci=pair<char, int>;
+using vvi=vector<vector<int>>;
+using vpii=vector<pair<int, int>>;
+using vpci=vector<pair<char, int>>;
+using vppiii=vector<pair<pair<int, int>, int>>;
+using vpipii=vector<pair<int, pair<int, int>>>;
+using dqpii=deque<pair<int, int>>;
+using vvpii=vector<vector<pair<int, int>>>;
+
+#define elif else if
+
+// const int INF(INT_MAX/2);
+// const ll mod(1000000007); // 10^9+7
+// const int offset(500000);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout<<fixed;
+    cout.precision(10);
+
+    int N;
+    cin>>N;
+    vpii cable(N+1);
+    for(int i=1; i<=N; ++i) cin>>cable[i].first>>cable[i].second;
+    sort(cable.begin(), cable.end(), [](const pii& x, const pii& y){
+        return x.first<y.first;
+    });
+    vi dp(N+1, 1);
+    int M(0);
+    for(int i=1; i<=N; ++i){
+        for(int j=1; j<i; ++j){
+            if(cable[i].second>cable[j].second) dp[i]=max(dp[i], dp[j]+1);
+        }
+        M=max(M, dp[i]);
+    }
+    cout<<N-M;
+
+    return 0;
+}
