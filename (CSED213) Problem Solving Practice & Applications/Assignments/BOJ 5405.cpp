0f20e34f45e2f25c26eef14468653800b54c237c
jkpxT14 <jkpxtfin@gmail.com>
2025-11-08 04:18:19 +0900

    Commit

--- (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 5405.cpp
+++ (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 5405.cpp
@@ -60,6 +60,31 @@ using mii=map<int, int>;
 // const ll mod(1000000007); // 10^9+7
 // const int offset(500000);
 
+pii Hilbert_curve(int order, int target){
+    if(order==1){
+        switch(target){
+            case 1:
+                return {0, 0};
+            case 2:
+                return {0, 10};
+            case 3:
+                return {10, 10};
+            case 4:
+                return {10, 0};
+        }
+    }
+    int div_num((int)pow(4, order-1)), correction(((int)pow(2, order))*5), x_symmetry((((int)pow(2, order))-1)*5), y_symmetry((((int)pow(2, order-1))-1)*5);
+    pii p(Hilbert_curve(order-1, ((target-1)%div_num)+1));
+    if(target<=div_num) return {p.second, p.first};
+    elif(target<=div_num*2) return {p.first, p.second+correction};
+    elif(target<=div_num*3) return {p.first+correction, p.second+correction};
+    else return {2*x_symmetry-p.second, 2*y_symmetry-p.first};
+}
+
+int dist(pii p, pii q){
+    return (int)(sqrt(pow(p.first-q.first, 2)+pow(p.second-q.second, 2))+0.5);
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -67,6 +92,14 @@ int main(){
 
     cout<<fixed;
     cout.precision(10);
+
+    int T;
+    cin>>T;
+    while(T--){
+        int n, h, o;
+        cin>>n>>h>>o;
+        cout<<dist(Hilbert_curve(n, h), Hilbert_curve(n, o))<<"\n";
+    }
     
     return 0;
 }
