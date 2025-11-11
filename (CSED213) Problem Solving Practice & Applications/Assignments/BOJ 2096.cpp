60417e5faa58a3693cb4167a286def2abe322b2b
jkpxT14 <jkpxtfin@gmail.com>
2025-11-11 17:49:23 +0900

    Commit

--- (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 2096.cpp
+++ (CSED213) Problem Solving Practice & Applications/Assignments/BOJ 2096.cpp
@@ -66,6 +66,29 @@ int main(){
 
     cout<<fixed;
     cout.precision(10);
+
+    int N;
+    cin>>N;
+    vi dp_max(3);
+    for(int i(0); i<3; ++i) cin>>dp_max[i];
+    vi dp_min(dp_max);
+    for(int i(1); i<N; ++i){
+        vi line(3);
+        for(int j(0); j<3; ++j) cin>>line[j];
+
+        vi prev(3);
+
+        prev=dp_max;
+        dp_max[0]=max(prev[0], prev[1])+line[0];
+        dp_max[1]=max(prev[0], max(prev[1], prev[2]))+line[1];
+        dp_max[2]=max(prev[1], prev[2])+line[2];
+
+        prev=dp_min;
+        dp_min[0]=min(prev[0], prev[1])+line[0];
+        dp_min[1]=min(prev[0], min(prev[1], prev[2]))+line[1];
+        dp_min[2]=min(prev[1], prev[2])+line[2];
+    }
+    cout<<*max_element(dp_max.begin(), dp_max.end())<<" "<<*min_element(dp_min.begin(), dp_min.end());
     
     return 0;
 }
