af25782d477ddb615fa87d5665a7915104a72138
jkpxT14 <jkpxtfin@gmail.com>
2025-11-20 23:34:13 +0900

    Commit

---
+++ (CSED001) Competitive Programming/UDPC/2023/E.cpp
@@ -0,0 +1,103 @@
+// BOJ 27922: 현대모비스 입사 프로젝트
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <tuple>
+#include <climits>
+#include <limits>
+#include <cmath>
+#include <bitset>
+#include <functional>
+#include <cassert>
+
+using namespace std;
+
+using ll=long long;
+using pii=pair<int, int>;
+using pll=pair<long long, long long>;
+using pci=pair<char, int>;
+using pipii=pair<int, pair<int, int>>;
+using ppiii=pair<pair<int, int>, int>;
+using vi=vector<int>;
+using vll=vector<long long>;
+using vd=vector<double>;
+using vb=vector<bool>;
+using vc=vector<char>;
+using vstr=vector<string>;
+using vpii=vector<pair<int, int>>;
+using vpll=vector<pair<long long, long long>>;
+using vpci=vector<pair<char, int>>;
+using vpipii=vector<pair<int, pair<int, int>>>;
+using vppiii=vector<pair<pair<int, int>, int>>;
+using vvi=vector<vector<int>>;
+using vvll=vector<vector<long long>>;
+using vvc=vector<vector<char>>;
+using vvpii=vector<vector<pair<int, int>>>;
+using vvvi=vector<vector<vector<int>>>;
+using vvvll=vector<vector<vector<long long>>>;
+using dqi=deque<int>;
+using dqll=deque<long long>;
+using dqpii=deque<pair<int, int>>;
+using pqi=priority_queue<int>;
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqll=priority_queue<long long>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+using mii=map<int, int>;
+
+#define elif else if
+const auto strnpos=string::npos;
+
+// const int INF(INT_MAX);
+// const ll mod(1000000007); // 10^9+7
+// const int offset(500000);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed;
+    cout.precision(10);
+
+    int N, K;
+    cin>>N>>K;
+    vi a(N), b(N), c(N), ab(N), ac(N), bc(N);
+    for(int i(0); i<N; ++i){
+        cin>>a[i]>>b[i]>>c[i];
+        ab[i]=a[i]+b[i];
+        ac[i]=a[i]+c[i];
+        bc[i]=b[i]+c[i];
+    }
+    sort(ab.begin(), ab.end(), greater<int>());
+    sort(ac.begin(), ac.end(), greater<int>());
+    sort(bc.begin(), bc.end(), greater<int>());
+    int ans(0), sum(0);
+    for(int i(0); i<K; ++i){
+        sum+=ab[i];
+    }
+    ans=max(ans, sum);
+    sum=0;
+    for(int i(0); i<K; ++i){
+        sum+=ac[i];
+    }
+    ans=max(ans, sum);
+    sum=0;
+    for(int i(0); i<K; ++i){
+        sum+=bc[i];
+    }
+    ans=max(ans, sum);
+    cout<<ans;
+    
+    return 0;
+}
