54925e9e5f530e5f57133651969a0bf941f3384e
jkpxT14 <jkpxtfin@gmail.com>
2025-12-12 21:31:55 +0900

    Commit

---
+++ (CSED001) Competitive Programming/Baekjoon OJ/BOJ 1068 (BFS).cpp
@@ -0,0 +1,144 @@
+// BOJ 1068: 트리
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+using pii=pair<int, int>;
+using pll=pair<long long, long long>;
+using pci=pair<char, int>;
+using pipii=pair<int, pair<int, int>>;
+using ppiii=pair<pair<int, int>, int>;
+using vi=vector<int>;
+using vll=vector<long long>;
+using vd=vector<double>;
+using vb=vector<bool>;
+using vc=vector<char>;
+using vstr=vector<string>;
+using vpii=vector<pair<int, int>>;
+using vpll=vector<pair<long long, long long>>;
+using vpci=vector<pair<char, int>>;
+using vpipii=vector<pair<int, pair<int, int>>>;
+using vppiii=vector<pair<pair<int, int>, int>>;
+using vvi=vector<vector<int>>;
+using vvll=vector<vector<long long>>;
+using vvb=vector<vector<bool>>;
+using vvc=vector<vector<char>>;
+using vvpii=vector<vector<pair<int, int>>>;
+using vvvi=vector<vector<vector<int>>>;
+using vvvll=vector<vector<vector<long long>>>;
+using dqi=deque<int>;
+using dqll=deque<long long>;
+using dqpii=deque<pair<int, int>>;
+using pqi=priority_queue<int>;
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqll=priority_queue<long long>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+using mii=map<int, int>;
+
+using vvvb=vector<vector<vector<bool>>>;
+using vvvvb=vector<vector<vector<vector<bool>>>>;
+using vvvvi=vector<vector<vector<vector<int>>>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first;
+    x.second+=y.second;
+    return x;
+}
+
+template <typename TN>
+void print_1D(const vector<TN> &v, int type, int N){
+    for(int i(type); i<N+type; ++i){
+        cout<<v[i]<<' ';
+    }
+    cout<<'\n';
+}
+
+template <typename TN1, typename TN2>
+void print_1D(const vector<pair<TN1, TN2>> &v, int type, int N){
+    for(int i(type); i<N+type; ++i){
+        cout<<'('<<v[i].first<<", "<<v[i].second<<") ";
+    }
+    cout<<'\n';
+}
+
+template <typename TN>
+void print_2D(const vector<vector<TN>> &v, int type, int M, int N){
+    for(int i(type); i<M+type; ++i){
+        for(int j(type); j<N+type; ++j){
+            cout<<v[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+}
+
+int mod(int x, int modulus){ // modulo
+    return ((x%modulus)+modulus)%modulus;
+}
+
+// const int INF(INT_MAX/4);
+// const ll mod(1000000007);
+// const int offset(500000);
+// const vpii delta{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int N, root, dlt, leaf(0); // dlt=delete
+vvi tree;
+vb vst;
+
+void bfs(int start){
+    dqi q;
+    q.push_back(start);
+    vst[start]=true;
+    while(!q.empty()){
+        int cur(q.front()); q.pop_front();
+        for(int nxt: tree[cur]){
+            if(!vst[nxt]){
+                q.push_back(nxt);
+                vst[nxt]=true;
+            }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    cin>>N;
+    tree.resize(N);
+    vst.resize(N, false);
+    for(int i(0); i<N; ++i){
+        int parent;
+        cin>>parent;
+        if(parent==-1){
+            root=i;
+        }
+        else{
+            tree[parent].push_back(i);
+        }
+    }
+    cin>>dlt;
+
+    bfs(dlt);
+    for(int i(0); i<N; ++i){
+        if(!vst[i] && (tree[i].size()==0 || (tree[i].size()==1 && tree[i][0]==dlt))){
+            ++leaf;
+        }
+    }
+    cout<<leaf;
+
+    return 0;
+}
