03afc8d51bf2ccc475aafaf61c7fb8c68d170e26
jkpxT14 <jkpxtfin@gmail.com>
2026-01-22 15:09:02 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 10828.cpp
@@ -0,0 +1,95 @@
+// BOJ 10828: 스택
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+using mii=map<int, int>;
+using umii=unordered_map<int, int>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first;
+    x.second+=y.second;
+    return x;
+}
+
+template <typename TN>
+void print_1D(const vector<TN> &v, int i1, int i2){
+    for(int i(i1); i<=i2; ++i){
+        cout<<v[i]<<' ';
+    }
+    cout<<'\n';
+}
+
+template <typename TN1, typename TN2>
+void print_1D(const vector<pair<TN1, TN2>> &v, int i1, int i2){
+    for(int i(i1); i<=i2; ++i){
+        cout<<'('<<v[i].first<<", "<<v[i].second<<") ";
+    }
+    cout<<'\n';
+}
+
+template <typename TN>
+void print_2D(const vector<vector<TN>> &v, int i1, int i2, int j1, int j2){
+    for(int i(i1); i<=i2; ++i){
+        for(int j(j1); j<=j2; ++j){
+            cout<<v[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const vector<pair<int, int>> delta{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}, UDLR{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N; cin>>N;
+    deque<int> stk;
+    for(int i(0); i<N; ++i){
+        string order; cin>>order;
+        if(order=="push"){
+            int X; cin>>X;
+            stk.push_back(X);
+        } elif(order=="pop"){
+            if(!stk.empty()){
+                cout<<stk.back()<<'\n';
+                stk.pop_back();
+            } else{
+                cout<<-1<<'\n';
+            }
+        } elif(order=="size"){
+            cout<<stk.size()<<'\n';
+        } elif(order=="empty"){
+            cout<<int(stk.empty())<<'\n';
+        } elif(order=="top"){
+            if(!stk.empty()){
+                cout<<stk.back()<<'\n';
+            } else{
+                cout<<-1<<'\n';
+            }
+        }
+    }
+
+    return 0;
+}
