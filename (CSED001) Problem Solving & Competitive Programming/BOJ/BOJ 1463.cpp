a0fdac548a1046cddb634dd8de308a213cd970ca
jkpxT14 <jkpxtfin@gmail.com>
2026-02-14 15:41:01 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 1463.cpp
@@ -0,0 +1,53 @@
+// BOJ 1463: 1로 만들기
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+const int INF(INT_MAX);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N; cin>>N;
+    vector<int> dp(N+1, INF);
+    dp[1]=0;
+    for(int i(2); i<=N; ++i){
+        if(i%3==0){
+            dp[i]=min(dp[i], dp[i/3]+1);
+        }
+        if(i%2==0){
+            dp[i]=min(dp[i], dp[i/2]+1);
+        }
+        dp[i]=min(dp[i], dp[i-1]+1);
+    }
+    cout<<dp[N];
+
+    return 0;
+}
