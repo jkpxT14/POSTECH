36e2124c2f9c86852f4558c4edf7656a7efec8ee
jkpxT14 <jkpxtfin@gmail.com>
2026-02-16 14:28:35 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 15654.cpp
@@ -0,0 +1,79 @@
+// BOJ 15654: N과 M (5)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int N, M;
+vector<int> num;
+vector<bool> vst;
+
+void DFS(vector<int> sq){
+    if(sq.size()==M){
+        for(int i(0); i<=M-1; ++i){
+            cout<<sq[i]<<" \n"[i==M-1];
+        }
+        return;
+    }
+    if(sq.empty()){
+        for(int i(1); i<=N; ++i){
+            sq.push_back(num[i]);
+            vst[i]=true;
+            DFS(sq);
+            sq.pop_back();
+            vst[i]=false;
+        }
+    } else{
+        for(int i(1); i<=N; ++i){
+            if(!vst[i]){
+                sq.push_back(num[i]);
+                vst[i]=true;
+                DFS(sq);
+                sq.pop_back();
+                vst[i]=false;
+            }
+        }
+    }
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    cin>>N>>M;
+    num.resize(N+1);
+    for(int i(1); i<=N; ++i) cin>>num[i];
+    sort(num.begin()+1, num.end());
+    vst.resize(N+1, false);
+    DFS({});
+
+    return 0;
+}
