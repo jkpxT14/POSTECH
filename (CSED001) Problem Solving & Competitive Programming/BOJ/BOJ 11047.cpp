4b4c83235a3eb971db210d9d1745f9250e6b92f1
jkpxT14 <jkpxtfin@gmail.com>
2026-02-18 18:08:58 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 11047.cpp
@@ -0,0 +1,53 @@
+// BOJ 11047: 동전 0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N, K; cin>>N>>K;
+    vector<int> A(N+1); for(int i(1); i<=N; ++i) cin>>A[i];
+    int ans(0);
+    for(int i(N); i>=0; --i){
+        if(K>=A[i]){
+            ans+=K/A[i];
+            K%=A[i];
+            if(!K){
+                break;
+            }
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
