edb63b5051f192048dceaf8f7ed7471777681597
jkpxT14 <jkpxtfin@gmail.com>
2026-02-19 13:22:40 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 2579.cpp
@@ -0,0 +1,48 @@
+// BOJ 2579: 계단 오르기
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N; cin>>N;
+    vector<int> stair(N+1); for(int i(1); i<=N; ++i) cin>>stair[i];
+    vector<int> dp(N+1);
+    dp[0]=0; dp[1]=stair[1]; dp[2]=stair[1]+stair[2];
+    for(int i(3); i<=N; ++i){
+        dp[i]=max(dp[i-2]+stair[i], dp[i-3]+stair[i-1]+stair[i]);
+    }
+    cout<<dp[N];
+
+    return 0;
+}
