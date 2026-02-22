bcb1f176837709b0d05ebaa218688ed4a13820d3
Jungwoo Kim (jkpxT14) <jkpxtfin@gmail.com>
2026-02-22 15:59:25 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 11726.cpp
@@ -0,0 +1,48 @@
+// BOJ 11726: 2×n 타일링
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+const int mdls(10007);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int n; cin>>n;
+    vector<int> dp(n+1);
+    dp[1]=1; dp[2]=2;
+    for(int i(3); i<=n; ++i){
+        dp[i]=dp[i-1]+dp[i-2];
+        dp[i]%=mdls;
+    }
+    cout<<dp[n];
+
+    return 0;
+}
