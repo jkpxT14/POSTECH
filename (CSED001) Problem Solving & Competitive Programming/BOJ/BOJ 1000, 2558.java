749eb2893d890e1a8997645ea68e1829865800cd
jkpxT14 <jkpxtfin@gmail.com>
2026-02-25 14:34:34 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/BOJ/BOJ 1000, 2558.java
@@ -0,0 +1,15 @@
+// BOJ 1000: A+B, BOJ 2558: A+B - 2
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.print(A+B);
+
+        sc.close();
+    }
+}
