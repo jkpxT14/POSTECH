64d93dc0b59f9dbe574963b479de4eb7e33efa4e
jkpxT14 <jkpxtfin@gmail.com>
2026-03-23 17:50:46 +0900

    Commit

--- (CSED001) Problem Solving & Competitive Programming/UDPC/2025/C.cpp
+++ (CSED001) Problem Solving & Competitive Programming/UDPC/2025/C.cpp
@@ -1,1 +1,59 @@
 // BOJ 33691: Arkain 대시보드
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+using pqig=priority_queue<int, vector<int>, greater<int>>;
+using pqllg=priority_queue<long long, vector<long long>, greater<long long>>;
+
+#define elif else if
+
+const auto strnpos=string::npos;
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N; cin>>N;
+    map<string, int> priority;
+    for(int i(0); i<=N-1; ++i){
+        string container; cin>>container;
+        priority[container]=i;
+    }
+    int K; cin>>K;
+    for(int i(0); i<=K-1; ++i){
+        string container; cin>>container;
+        priority[container]+=N;
+    }
+    vector<pair<int, string>> dashboard;
+    for(auto &p: priority){
+        dashboard.push_back({p.second, p.first});
+    }
+    sort(dashboard.begin(), dashboard.end(), greater<>());
+    for(pair<int, string> &p: dashboard){
+        cout<<p.second<<'\n';
+    }
+
+    return 0;
+}
