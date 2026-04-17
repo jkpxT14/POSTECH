9b7839afdf76b145b8cc87f1ef1ded3409573c24
jkpxT14 <jkpxtfin@gmail.com>
2026-04-18 01:15:18 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/CSES/Missing Number Sol 1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+using ull=unsigned long long;
+
+#define elif else if
+
+const auto strnpos(string::npos);
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int n; cin>>n;
+    vector<bool> v(n+1, false);
+    for(int i(1); i<=n-1; ++i){
+        int m; cin>>m;
+        v[m]=true;
+    }
+    for(int i(1); i<=n; ++i){
+        if(!v[i]){
+            cout<<i;
+            return 0;
+        }
+    }
+}
