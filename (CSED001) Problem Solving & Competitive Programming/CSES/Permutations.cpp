d55bcec4275b8af59a5eb4ae520dacce9b158f76
jkpxT14 <jkpxtfin@gmail.com>
2026-04-18 01:15:18 +0900

    Commit

---
+++ (CSED001) Problem Solving & Competitive Programming/CSES/Permutations.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+using ull=unsigned long long;
+
+#define elif else if
+
+const auto strnpos(string::npos);
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int n; cin>>n;
+    if(n==1){
+        cout<<n;
+    } elif(n==2 || n==3){
+        cout<<"NO SOLUTION";
+    } else{
+        for(int i(1); i<=n/2; ++i){
+            cout<<2*i<<' ';
+        }
+        for(int i(1); i<=(n%2==0?n/2:n/2+1); ++i){
+            cout<<2*i-1<<' ';
+        }
+    }
+
+    return 0;
+}
---
+++ (CSED001) Problem Solving & Competitive Programming/CSES/Repetitions.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+using ull=unsigned long long;
+
+#define elif else if
+
+const auto strnpos(string::npos);
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator+(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> & operator+=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+// const ll mdls(1000000007LL);
+// const int INF(INT_MAX/4);
+// const int offset(500000);
+// const array<pair<int, int>, 4> delta{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    string DNA; cin>>DNA;
+    int ans(1), cnt(1);
+    for(int i(1); i<=DNA.size()-1; ++i){
+        if(DNA[i]==DNA[i-1]){
+            ++cnt;
+        } else{
+            ans=max(ans, cnt);
+            cnt=1;
+        }
+    }
+    ans=max(ans, cnt);
+    cout<<ans;
+
+    return 0;
+}
