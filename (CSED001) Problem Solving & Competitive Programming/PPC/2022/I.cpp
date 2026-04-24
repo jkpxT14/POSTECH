050de7d8d05ea298ed9165f68e7e2f89686d958f
jkpxT14 <jkpxtfin@gmail.com>
2026-04-24 17:54:38 +0900

    Commit

--- (CSED001) Problem Solving & Competitive Programming/PPC/2022/I.cpp
+++ (CSED001) Problem Solving & Competitive Programming/PPC/2022/I.cpp
@@ -34,6 +34,66 @@ int main(){
     cout<<fixed<<setprecision(10);
 
     int N, M; cin>>N>>M;
-
+    vector<vector<char>> present(N, vector<char>(M));
+    for(int i(0); i<=N-1; ++i){
+        for(int j(0); j<=M-1; ++j){
+            cin>>present[i][j];
+        }
+    }
+    int D; cin>>D;
+    vector<vector<char>> future(N, vector<char>(M));
+    for(int i(0); i<=N-1; ++i){
+        for(int j(0); j<=M-1; ++j){
+            cin>>future[i][j];
+        }
+    }
+    vector<pair<int, int>> within;
+    for(int dx(-D); dx<=D; ++dx){
+        for(int dy(-D); dy<=D; ++dy){
+            if(abs(dx)+abs(dy)<=D){
+                within.push_back({dx, dy});
+            }
+        }
+    }
+    deque<pair<int, int>> q;
+    vector<vector<bool>> vst(N, vector<bool>(M, false));
+    for(int i(0); i<=N-1; ++i){
+        for(int j(0); j<=M-1; ++j){
+            if(present[i][j]=='O' && future[i][j]=='X'){
+                cout<<"NO";
+                return 0;
+            }
+            if(present[i][j]=='O'){
+                q.push_back({i, j});
+                vst[i][j]=true;
+            }
+        }
+    }
+    while(!q.empty()){
+        pair<int, int> cur(q.front()); q.pop_front();
+        for(pair<int, int> d: within){
+            pair<int, int> nxt(cur+d);
+            if(nxt.first<0 || nxt.first>=N || nxt.second<0 || nxt.second>=M){
+                continue;
+            }
+            if(vst[nxt.first][nxt.second]){
+                continue;
+            }
+            if(future[nxt.first][nxt.second]=='X'){
+                continue;
+            }
+            q.push_back(nxt);
+            vst[nxt.first][nxt.second]=true;
+        }
+    }
+    for(int i(0); i<=N-1; ++i){
+        for(int j(0); j<=M-1; ++j){
+            if(future[i][j]=='O' && !vst[i][j]){
+                cout<<"NO";
+                return 0;
+            }
+        }
+    }
+    cout<<"YES";
     return 0;
 }
--- (CSED001) Problem Solving & Competitive Programming/PPC/2023/J1.cpp
+++ (CSED001) Problem Solving & Competitive Programming/PPC/2023/J1.cpp
@@ -1,1 +1,79 @@
 // BOJ 28106: 나무 타기
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+using ull=unsigned long long;
+
+#define elif else if
+
+const auto strnpos(string::npos);
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> operator +(const pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    return {x.first+y.first, x.second+y.second};
+}
+
+template <typename TN1, typename TN2>
+pair<TN1, TN2> &operator +=(pair<TN1, TN2> &x, const pair<TN1, TN2> &y){
+    x.first+=y.first; x.second+=y.second;
+    return x;
+}
+
+const int mdls(998244353);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<fixed<<setprecision(10);
+
+    int N; cin>>N;
+    vector<int> P(N+1);
+    vector<vector<int>> C(N+1);
+    int root(0);
+    for(int i(1); i<=N; ++i){
+        cin>>P[i];
+        if(P[i]==0){
+            root=i;
+        } else{
+            C[P[i]].push_back(i);
+        }
+    }
+    vector<int> A(N+1); for(int i(1); i<=N; ++i) cin>>A[i];
+    vector<int> D(N+1);
+    vector<int> order;
+    function<void(int)> DFS=[&](int cur){
+        order.push_back(cur);
+        for(int nxt: C[cur]){
+            D[nxt]=D[cur]+1;
+            DFS(nxt);
+        }
+    };
+    DFS(root);
+    vector<int> DP(N+1, 0);
+    DP[root]=1;
+    for(int nd: order){
+        if(nd==root){
+            continue;
+        }
+        for(int p(P[nd]); p!=0; p=P[p]){
+            if(D[nd]-D[p]<=A[p]){
+                DP[nd]+=(DP[p]%mdls);
+                DP[nd]%=mdls;
+            }
+        }
+    }
+    int ans(0);
+    for(int i(1); i<=N; ++i){
+        if(C[i].empty()){
+            ans+=DP[i];
+            ans%=mdls;
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
